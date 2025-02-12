76ced470fb080378d54423109c3982d297fd7a4f
Shita Zeny <shita0639@gmail.com>
2025-02-12 08:19:18 +0700

    feat: add middleware and JWT in hono-node #1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { Routes } from './routes'
 
-const app = new Hono()
+const app = new Hono().basePath('/api')
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.route('/posts', Routes)
 
 const port = 3000
 console.log(`Server is running on http://localhost:${port}`)
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,36 @@
 //import hono
 import { Hono } from 'hono';
 import { createPost, deletePost, getPostById, getPosts, updatePost } from '../controller/PostControllers';
+import { basicAuth } from 'hono/basic-auth'
+import { jwt } from 'hono/jwt'
+import type { JwtVariables } from 'hono/jwt'
+
+type Variables = JwtVariables
+const router = new Hono<{ Variables: Variables }>()
+
+router.use(
+  '/auth/*',
+  basicAuth({
+    username: 'hono',
+    password: 'acoolproject',
+  })
+)
+
+router.get('/auth/page', (c) => {
+  return c.text('You are authorized')
+})
 
 
-//import controller
+router.use(
+  '/user/*',
+  jwt({
+    secret: 'it-is-very-secret',
+  })
+)
 
-
-//inistialize router
-const router = new Hono()
+router.get('/user/page', (c) => {
+  return c.text('You are authorized')
+})
 
 //routes posts index
 router.get('/', (c) => getPosts(c));
